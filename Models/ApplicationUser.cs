49f1bd26f5631b40b421f7e7e99f79ce8ebcde31
Ling Liu <lingliu1123@gmail.com>
2022-03-24 15:50:03 -0400

    Change List to ICollection in User

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace FinalProject_AdvancedDatabaseAndORMConcepts_SlackOverload.Models
     public class ApplicationUser : IdentityUser
     {
         public int Reputation { get; set; }
-        public virtual List<Question> Questions { get; set; }
-        public virtual List<Answer> Answers { get; set; }
-        public virtual List<Comment> Comments { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+        public virtual ICollection<Answer> Answers { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
